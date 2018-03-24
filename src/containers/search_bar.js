c579d651092a4946b58211f9a5cf50f5f537b60f
aayush1408 <aayushjaiswal0114@gmail.com>
2018-03-24 22:05:06 +0530

    sparklines added

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,39 +1,39 @@
 import React from 'react';
-import {connect} from 'react-redux';
-import {bindActionCreators} from 'redux';
-import {fetchWeather} from '../actions/index.js';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index.js';
 
-class SearchBar extends React.Component{
-    constructor(props){
+class SearchBar extends React.Component {
+    constructor(props) {
         super(props);
-        this.state = {term:' '}
+        this.state = { term: ' ' }
     }
-    onInputChange(e){
+    onInputChange(e) {
         this.setState({
-            term:e.target.value
+            term: e.target.value
         })
     }
-    onSubmitForm(e){
+    onSubmitForm(e) {
         e.preventDefault();
         this.props.fetchWeather(this.state.term);
-        this.setState({term:''});
+        this.setState({ term: '' });
     }
-    render(){
-        return(
-               <form className="input-group" onSubmit={this.onSubmitForm.bind(this)} >
-                   <input 
-                   placeholder="Enter your favourite city"
-                   className="form-control"
-                   value={this.state.term}
-                   onChange={this.onInputChange.bind(this)}
-                   />
-                   <button className="btn btn-sm btn-primary">Submit</button>
-               </form>
-               );
+    render() {
+        return (
+            <form className="input-group" onSubmit={this.onSubmitForm.bind(this)} >
+                <input
+                    placeholder="Enter your favourite city"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange.bind(this)}
+                />
+                <button className="btn btn-sm btn-primary">Submit</button>
+            </form>
+        );
     };
-    
+
 }
-function mapDispatchToprops(dispatch){
-        return bindActionCreators({fetchWeather},dispatch);
-    }
-export default connect(null,mapDispatchToprops)(SearchBar);
+function mapDispatchToprops(dispatch) {
+    return bindActionCreators({ fetchWeather }, dispatch);
+}
+export default connect(null, mapDispatchToprops)(SearchBar);
--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,38 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
-import {connect} from 'react-redux';
-
-
-class WeatherList extends React.Component{
-  renderWeather(cityData){
-    const name = cityData.city.name;
-    const temp = cityData.list.map(weather => weather.main.temp);
-    return(
-        <tr key={name}> 
-          <td>{name}</td>
-          
-        </tr>
-           );
-  }
-    render(){
-      return(
-            <table className="table table-hover">
-                <thead>
-                  <tr>
-                    <th> City </th>
-                    <th> Temperature </th>
-                    <th> Pressure </th>
-                    <th> HUmidity </th>
-                  </tr>
-                </thead>
-                <tbody> 
-                  {this.props.weather.map(this.renderWeather)}
-                </tbody>
-            </table>
-             )
-    }
+class WeatherList extends React.Component {
+	renderWeather(cityData) {
+		const name = cityData.city.name;
+		const temps = cityData.list.map(weather => weather.main.temp);
+		return (
+			<tr key={name}>
+				<td>{name}</td>
+				<td>
+					<Sparklines height={120} width={180} data={temps}>
+						<SparklinesLine color="red" />
+					</Sparklines>
+				</td>
+			</tr>
+		);
+	}
+	render() {
+		return (
+			<table className="table table-hover">
+				<thead>
+					<tr>
+						<th> City </th>
+						<th> Temperature </th>
+						<th> Pressure </th>
+						<th> HUmidity </th>
+					</tr>
+				</thead>
+				<tbody>{this.props.weather.map(this.renderWeather)}</tbody>
+			</table>
+		);
+	}
 }
-function mapStateToProps ({weather}){
-  return {weather};
+function mapStateToProps({ weather }) {
+	return { weather };
 }
-export default connect(mapStateToProps)(WeatherList);   
+export default connect(mapStateToProps)(WeatherList);
